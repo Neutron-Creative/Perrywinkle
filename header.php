544968595f01db1b624b815e57b49d8620c04306
Jimmy Bisenius <jimmyb@neutroncreative.com>
2018-07-08 16:57:44 -0400

    Added NPM and base header

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width,initial-scale=1">
+	</head>
+	<body>
+		<nav>
+			<div class="brand">
+				<?php the_custom_logo(); ?>
+				<?php bloginfo( 'name' ); ?>
+			</div>
+		</nav>
